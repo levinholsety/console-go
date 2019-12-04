ad1488dcc85f20981f2db1cc122099b1bc95f0d9
Rui Li <levinholsety@hotmail.com>
2019-12-04 10:41:45 +0800

    Add time duration to progress bar.

--- console/progress_bar.go
+++ console/progress_bar.go
@@ -3,15 +3,20 @@ package console
 import (
 	"fmt"
 	"strings"
+	"time"
+
+	"github.com/levinholsety/common-go/timeutil"
 )
 
 type ProgressBar struct {
+	epoch time.Time
 	Width int
 	Total int
 }
 
 func NewProgressBar(total int) *ProgressBar {
 	return &ProgressBar{
+		epoch: time.Now(),
 		Width: 50,
 		Total: total,
 	}
@@ -21,5 +26,5 @@ func (p *ProgressBar) Progress(current int) {
 	progressLen := current * p.Width / p.Total
 	progress := strings.Repeat("=", progressLen)
 	blanks := strings.Repeat(" ", p.Width-progressLen)
-	fmt.Printf("\r[%s%s] %d%%", progress, blanks, current*100/p.Total)
+	fmt.Printf("\r[%s%s] %d%% %s", progress, blanks, current*100/p.Total, timeutil.FormatDuration(time.Now().Sub(p.epoch)))
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,7 @@ module github.com/levinholsety/console-go
 
 go 1.13
 
-replace (
-	golang.org/x/crypto => github.com/golang/crypto v0.0.0-20191202143827-86a70503ff7e
-	golang.org/x/sys => github.com/golang/sys v0.0.0-20191128015809-6d18c012aee9
+require (
+	github.com/levinholsety/common-go v0.0.0-20191204021126-baa3f4159084
+	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9
 )
-
-require golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9
