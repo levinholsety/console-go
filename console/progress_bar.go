8aab407af8355c9e287f07f90f186558d36b7c37
Rui Li <levinholsety@hotmail.com>
2019-12-11 10:54:32 +0800

    Update reference of common-go.

--- console/progress_bar.go
+++ console/progress_bar.go
@@ -5,15 +5,17 @@ import (
 	"strings"
 	"time"
 
-	"github.com/levinholsety/common-go/timeutil"
+	"github.com/levinholsety/common-go/commtime"
 )
 
+// ProgressBar can print progress bar in console.
 type ProgressBar struct {
 	epoch time.Time
 	Width int
 	Total int
 }
 
+// NewProgressBar creates an instance of ProgressBar.
 func NewProgressBar(total int) *ProgressBar {
 	return &ProgressBar{
 		epoch: time.Now(),
@@ -22,9 +24,10 @@ func NewProgressBar(total int) *ProgressBar {
 	}
 }
 
+// Progress changes progress.
 func (p *ProgressBar) Progress(current int) {
 	progressLen := current * p.Width / p.Total
 	progress := strings.Repeat("=", progressLen)
 	blanks := strings.Repeat(" ", p.Width-progressLen)
-	fmt.Printf("\r[%s%s] %d%% %s", progress, blanks, current*100/p.Total, timeutil.FormatDuration(time.Now().Sub(p.epoch)))
+	fmt.Printf("\r[%s%s] %d%% %s", progress, blanks, current*100/p.Total, commtime.FormatDuration(time.Now().Sub(p.epoch)))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/levinholsety/console-go
 go 1.13
 
 require (
-	github.com/levinholsety/common-go v0.0.0-20191204021126-baa3f4159084
+	github.com/levinholsety/common-go v0.0.0-20191211025115-020201b85646
 	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9
 )
